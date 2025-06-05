8495198a60a7922801448117b8d199d85c19e531
JAAS210299 <86979893+JAAS210299@users.noreply.github.com>
2025-06-05 12:34:49 +0200

    Update app.py
    agregada ruta par-impar

--- src/app.py
+++ src/app.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
+@app.route('/par-impar')
+def par_impar():
+    numero = 10  # puedes cambiar este valor o pasarlo como parámetro
+    if numero % 2 == 0:
+        return f"{numero} es par"
+    else:
+        return f"{numero} es impar"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
